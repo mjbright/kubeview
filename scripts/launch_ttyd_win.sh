b95dd72adf157aed1bb37ce0dbcb7315c9b37f79
Mike Bright <github.com@mjbright.net>
2018-09-17 15:35:43 +0200

    Fixed sourcing of ttyd.bashrc

--- scripts/launch_ttyd_win.sh
+++ scripts/launch_ttyd_win.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-TTYD_BASHRC=~/.dotfiles/ttyd.bashrc
+#TTYD_BASHRC=~/.dotfiles/ttyd.bashrc
+TTYD_BASHRC=~/.dotfiles.MSYS/ttyd.bashrc
+TTYD_BASHRC=$(readlink -f ~/.dotfiles.MSYS/ttyd.bashrc)
 
 #TTYD_OPTIONS="-O -m 1 -S -d 7"
 TTYD_OPTIONS="-O -m 1 -d 7"
@@ -14,18 +16,21 @@ die() {
 
 # cd $(dirname $0)
 
-[ ! -d $TTYD_DIR ] && die "No such dir <$TTYD_DIR>"
+[ ! -d $TTYD_DIR ] && die "[$PWD] No such dir <$TTYD_DIR>"
 # cd ~/z/bin/win64/ttyd/
 cd $TTYD_DIR
+
+cp -a $TTYD_BASHRC .tmp.ttyd.bashrc
+TMP_TTYD_BASHRC=.tmp.ttyd.bashrc
 
 pwd
 ls -altr
 
-[ ! -x ttyd.exe ] && die "No such executable <ttyd.exe>"
-[ ! -f $TTYD_BASHRC ] && die "No such rc file <$TTYD_BASHRC>"
+[ ! -x ttyd.exe ] && die "[$PWD] No such executable <ttyd.exe>"
+[ ! -f $TMP_TTYD_BASHRC ] && die "[$PWD] No such rc file <$TMP_TTYD_BASHRC>"
 
-export XXX=yyy
-CMD="/c/tools/msys64/msys2.exe ./ttyd.exe $TTYD_OPTIONS -p 9001 bash --rcfile $TTYD_BASHRC"
+#export XXX=yyy
+CMD="/c/tools/msys64/msys2.exe ./ttyd.exe $TTYD_OPTIONS -p 9001 bash --rcfile $TMP_TTYD_BASHRC"
 
 echo $CMD
 $CMD
