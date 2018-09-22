2c29baa47254cad3c0fba8a49f5e9793d2bb1e58
Mike Bright <github.com@mjbright.net>
2018-09-22 16:38:24 +0200

    Added inspect() function

--- js/kubeview.js
+++ js/kubeview.js
@@ -940,6 +940,8 @@ const createModalText = (type, object, href_content, id, markup) => {
 		`;
 
 
+    const inspectText=inspect(object, 5);
+
     const modalText=`
     <a class="modal_href" href="#${id}_modal">${href_content}</a>
 
@@ -947,11 +949,68 @@ const createModalText = (type, object, href_content, id, markup) => {
       <div>
         <a href="#${id}_close_modal" title="Close" class="close">X</a>
 	${content}
+        <div class="scroll_auto"> ${inspectText} </div>
       </div>
       </div>`;
 
     return modalText;
 };
+
+const inspect = (obj, maxLevels, level) => {
+    var str = '', type, msg;
+
+    // Start Input Validations
+    // Don't touch, we start iterating at level zero
+    if(level == null)  level = 0;
+
+    // At least you want to show the first level
+    if(maxLevels == null) maxLevels = 1;
+    if(maxLevels < 1)     
+        return '<font color="red">Error: Levels number must be > 0</font>';
+
+    // We start with a non null object
+    if(obj == null)
+        return '<font color="red">Error: Object <b>NULL</b></font>';
+    // End Input Validations
+
+    // Each Iteration must be indented
+    str += '<ul style="list-style: disc outside none;" >';
+
+    // Start iterations for all objects in obj
+    for(var property in obj) {
+      try {
+          // Show "property" and "string value" if string
+          // WAS: Show "property" and "type property"
+          type =  typeof(obj[property]);
+          if (type == 'string') {
+              value = obj[property];
+              str += '<li> ' + property + ( (value==null)?(': <b>null</b>'):(': ' + value)) + '</li>';
+          } else {
+              str += '<li> ' + property + ( (value==null)?(': <b>null</b>'):('')) + '</li>';
+          }
+          //str += '<li>(' + type + ') ' + property + ( (obj[property]==null)?(': <b>null</b>'):('')) + '</li>';
+
+          // We keep iterating if this property is an Object, non null
+          // and we are inside the required number of levels
+          if((type == 'object') && (obj[property] != null) && (level+1 < maxLevels))
+              str += inspect(obj[property], maxLevels, level+1);
+      }
+      catch(err) {
+          // Are there some properties in obj we can't access? Print it red.
+          if(typeof(err) == 'string') msg = err;
+          else if(err.message)        msg = err.message;
+          else if(err.description)    msg = err.description;
+          else                        msg = 'Unknown';
+
+          str += '<li><font color="red">(Error) ' + property + ': ' + msg +'</font></li>';
+      }
+    }
+
+    // Close indent
+    str += '</ul>';
+
+    return str;
+}
 
 const redrawTopMenu = () => {
     //----- Build up namespace dropdown menu:
