7d34f63c2bdb167666cf871b740801fbac12a2dc
Michael Bright <github.com@mjbright.net>
2018-10-01 16:02:30 +0200

    Reformatted inspect code

--- js/kubeview.js
+++ js/kubeview.js
@@ -1027,15 +1027,15 @@ const inspect = (obj, maxLevels, level) => {
 
     // Start Input Validations
     // Don't touch, we start iterating at level zero
-    if(level == null)  level = 0;
+    if (level == null)  level = 0;
 
     // At least you want to show the first level
-    if(maxLevels == null) maxLevels = 1;
-    if(maxLevels < 1)     
+    if (maxLevels == null) maxLevels = 1;
+    if (maxLevels < 1)     
         return "<font color=\"red\">Error: Levels number must be > 0</font>";
 
     // We start with a non null object
-    if(obj == null)
+    if (obj == null)
         return "<font color=\"red\">Error: Object <b>NULL</b></font>";
     // End Input Validations
 
@@ -1043,7 +1043,7 @@ const inspect = (obj, maxLevels, level) => {
     str += "<ul style=\"list-style: disc outside none;\" >";
 
     // Start iterations for all objects in obj
-    for(var property in obj) {
+    for (var property in obj) {
       try {
           // Show "property" and "string value" if string
           // WAS: Show "property" and "type property"
@@ -1054,18 +1054,17 @@ const inspect = (obj, maxLevels, level) => {
           } else {
               str += "<li> " + property + ( (value==null)?(": <b>null</b>"):("")) + "</li>";
           }
-          //str += '<li>(' + type + ') ' + property + ( (obj[property]==null)?(': <b>null</b>'):('')) + '</li>';
 
           // We keep iterating if this property is an Object, non null
           // and we are inside the required number of levels
-          if((type == "object") && (obj[property] != null) && (level+1 < maxLevels))
+          if ((type == "object") && (obj[property] != null) && (level+1 < maxLevels))
               str += inspect(obj[property], maxLevels, level+1);
       }
       catch(err) {
           // Are there some properties in obj we can't access? Print it red.
-          if(typeof(err) == "string") msg = err;
-          else if(err.message)        msg = err.message;
-          else if(err.description)    msg = err.description;
+          if (typeof(err) == "string") msg = err;
+          else if (err.message)        msg = err.message;
+          else if (err.description)    msg = err.description;
           else                        msg = "Unknown";
 
           str += "<li><font color=\"red\">(Error) " + property + ": " + msg +"</font></li>";
