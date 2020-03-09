c28b32448c0e594191a641f88218a48ad59ec833
Michael Bright <1880109+mjbright@users.noreply.github.com>
2020-03-09 06:09:03 +0100

    Corrected CLEANUP for new 2./3. ordering

--- yaml/CLEANUP.sh
+++ yaml/CLEANUP.sh
@@ -10,15 +10,15 @@ RUN() {
     $CMD
 }
 
-RUN kubectl delete -f ./3-DEMO_SERVICES/flask-service.yaml
+RUN kubectl delete -f ./2-DEMO_SERVICES/flask-service.yaml
 RUN kubectl delete -f ./0-DEMO_BASIC/flask-deployment.yaml
 #RUN kubectl delete -f ./0-DEMO_BASIC/k8sdemo-deployment.yaml
 #RUN kubectl delete -f ./0-DEMO_BASIC/k8sdemo-service.yaml
-RUN kubectl delete -f ./3-DEMO_SERVICES/redis-service.yaml
+RUN kubectl delete -f ./2-DEMO_SERVICES/redis-service.yaml
 RUN kubectl delete -f ./0-DEMO_BASIC/redis-deployment.yaml
 
-RUN kubectl delete -n test0 -f ./3-DEMO_SERVICES/flask-service.yaml
-RUN kubectl delete -n test0 -f ./3-DEMO_SERVICES/redis-service.yaml
+RUN kubectl delete -n test0 -f ./2-DEMO_SERVICES/flask-service.yaml
+RUN kubectl delete -n test0 -f ./2-DEMO_SERVICES/redis-service.yaml
 RUN kubectl delete -n test0 -f ./0-DEMO_BASIC/redis-deployment.yaml
 RUN kubectl delete -n test0 -f ./0-DEMO_BASIC/flask-deployment.yaml
 
@@ -33,12 +33,12 @@ exit 0
 ./0-DEMO_BASIC/redis-deployment.yaml
 ./1-DEMO_SCALING
 ./1-DEMO_SCALING/flask-deployment.yaml.r4.v1.yaml
-./2-DEMO_UPGRADE
-./2-DEMO_UPGRADE/flask-deployment.yaml.r4.v2.yaml
-./2-DEMO_UPGRADE/flask-deployment.yaml.r4.v3.yaml
-./2-DEMO_UPGRADE/flask-deployment.yaml.r4.v4.yaml
-./2-DEMO_UPGRADE/flask-deployment.yaml.r4.v5.yaml
-./3-DEMO_SERVICES
-./3-DEMO_SERVICES/flask-service.yaml
-./3-DEMO_SERVICES/redis-service.yaml
+./3-DEMO_UPGRADE
+./3-DEMO_UPGRADE/flask-deployment.yaml.r4.v2.yaml
+./3-DEMO_UPGRADE/flask-deployment.yaml.r4.v3.yaml
+./3-DEMO_UPGRADE/flask-deployment.yaml.r4.v4.yaml
+./3-DEMO_UPGRADE/flask-deployment.yaml.r4.v5.yaml
+./2-DEMO_SERVICES
+./2-DEMO_SERVICES/flask-service.yaml
+./2-DEMO_SERVICES/redis-service.yaml
 ./CLEANUP.sh
